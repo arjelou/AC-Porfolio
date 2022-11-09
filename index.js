1ee070bacb9c5699cd19733c0611b7fc1b11239d
Jelou <arjelou.jelou@gmail.com>
2022-11-09 17:26:22 +0800

    Update Pgaes and Fix Sticky navigation bar

--- index.js
+++ index.js
@@ -3,3 +3,33 @@ goTo = (keyword) => {
     const portfolio = document.querySelector(identifier);
     portfolio.scrollIntoView({behavior: 'smooth'});
 }
+
+// const navTagList = document.querySelectorAll("#navTag");
+// for (let i = 0; i < navTagList.length; i++) {
+//     navTagList[i].style.backgroundColor = "red";
+//     return navTagList;
+// }
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+
+function myFunction() {
+//     const navTagList = document.querySelectorAll("a");
+
+//     for (let i = 0; i < navTagList.length; i++) {
+//         navTagList[i].style.color = "RED";
+// }
+
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+   
+  } else {
+    navbar.classList.remove("sticky");
+    navTagList[i].style.color = "white";
+  }
+}
+
